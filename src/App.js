cef90b8554d7dda1d7a1ac14a4a16ef065d4ad52
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-08-07 14:35:24 +0530

    ContactDump

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
+import Nav from './components/Nav/Nav';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
+import Footer from './components/Footer/Footer';
 function App() {
   return (
     <div className="App">
-      
+
+<Router>
+    <Nav/>
+    <Routes>
+  <Route path="/" element={<Home />} />
+  <Route path="/about" element={<About/>} />
+  {/* <Route path="/services" element={<Services/>} />
+   */}
+  <Route path="/contact" element={<Contact/>} />
+</Routes>
+<Footer/>
+    </Router>
     </div>
   );
 }
